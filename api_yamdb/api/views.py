12e597542b5a3ed2f733213988faeef5c2304b4e
Aleksandr Usolcev <alexusolev@gmail.com>
2022-10-23 11:03:44 +0300

    fiex errors

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -11,8 +11,8 @@ from rest_framework.permissions import (AllowAny, IsAuthenticated,
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework_simplejwt.tokens import AccessToken
-
 from reviews.models import Category, Genre, Review, Title, User
+
 from .filters import TitleFilter
 from .mixins import CreateDestroyListMixin
 from .permissions import (AllowAdminOnly, AllowAdminOrReadOnly,
